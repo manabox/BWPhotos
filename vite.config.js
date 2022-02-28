cc2e76c7e32df3826285a24be091209195957cbb
Mana <hello@webcreatormana.com>
2022-02-28 11:45:44 +0900

    Changed base directory

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/BWPhotos/',
   build: {
     outDir: 'docs',
-    base: '/BWPhotos/',
   },
 })
