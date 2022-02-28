1e3ca0eab467fb46ca9b234c4e5d9fd9a8ff52d9
Mana <hello@webcreatormana.com>
2022-02-28 11:51:05 +0900

    Added github url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <p>
-                <small>&copy; 2022 <a href="https://webcreatorbox.com">Web Creator Box</a></small>
+                <small>&copy; 2022 <a href="https://webcreatorbox.com">Web Creator Box</a></small> | <a href="https://github.com/manabox/BWPhotos">GitHub</a>
             </p>
         </footer>
     );
